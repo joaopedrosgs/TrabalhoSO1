8688ed28a4b8a2206e3f886e9bc303396a740ee7
João Pedro Silva <jsangregorio@hotmail.com>
2017-11-07 11:30:44 -0200

    Formatação e pré-incremento

--- main.c
+++ main.c
@@ -1,22 +1,19 @@
-#include <stdio.h>
 #include <pthread.h>
-#include <stdlib.h>
 #include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-
-
 
 #define MAX_THREADS 25
 #define second 1000000
 
-int numero_h = 0, numero_o = 0, h_gerado = 0, o_gerado = 0, formados=0;
-pthread_mutex_t lock;  // mutex para travar acesso às variáveis acima
-sem_t sem_o, sem_h; // semaforos para travar hidrogenios e oxigenios
+int numero_h = 0, numero_o = 0, h_gerado = 0, o_gerado = 0, formados = 0;
+pthread_mutex_t lock; // mutex para travar acesso às variáveis acima
+sem_t sem_o, sem_h;   // semaforos para travar hidrogenios e oxigenios
 
-void* Oxigenio(void* v) {
+void *Oxigenio(void *v) {
   pthread_mutex_lock(&lock); // Pedindo acesso às variaveis
   numero_o++;
-
   printf("Hidrogenio: %d\tOxigenio: %d\t<-\tNovo Oxigenio \n", numero_h,
          numero_o);
 
@@ -26,18 +23,19 @@ void* Oxigenio(void* v) {
     sem_post(&sem_h);
     sem_post(&sem_h);
 
-    printf("H20 Formado(%d)\nThread retornando (Oxigenio)\n", formados++);
+    printf("***** H20 Formado(%d) *****\nThread retornando (Oxigenio)\n", ++formados);
 
     pthread_mutex_unlock(&lock); // Liberando acesso às variaveis
   } else {
     pthread_mutex_unlock(&lock); // Liberando acesso às variaveis
-    sem_wait(&sem_o); // Como o semaforo é iniciado em 0, todo oxigenio que chega aqui trava
+    sem_wait(&sem_o); // Como o semaforo é iniciado em 0, todo oxigenio que
+                      // chega aqui trava
     printf("Thread retornando (Oxigenio)\n");
   }
   return NULL;
 }
 
-void* Hidrogenio(void* v) {
+void *Hidrogenio(void *v) {
   pthread_mutex_lock(&lock); // Pedindo acesso às variaveis
   numero_h++;
 
@@ -47,15 +45,17 @@ void* Hidrogenio(void* v) {
     numero_o--;
     numero_h -= 2;
 
-    sem_post(&sem_o); // Liberamos um oxigenio e um hidrogenio (somados com mais esse hidrogenio que está executando)
+    sem_post(&sem_o); // Liberamos um oxigenio e um hidrogenio (somados com mais
+                      // esse hidrogenio que está executando)
     sem_post(&sem_h);
 
-    printf("H20 Formado(%d)\nThread retornando (Hidrogenio)\n", formados++);
-
+    printf("***** H20 Formado(%d) *****\nThread retornando (Hidrogenio)\n", ++formados);
+    
     pthread_mutex_unlock(&lock); // Liberando acesso às variaveis
   } else {
-    pthread_mutex_unlock(&lock);// Liberando acesso às variaveis
-    sem_wait(&sem_h); // Como o semaforo é iniciado em 0, todo oxigenio que chega aqui trava
+    pthread_mutex_unlock(&lock); // Liberando acesso às variaveis
+    sem_wait(&sem_h); // Como o semaforo é iniciado em 0, todo oxigenio que
+                      // chega aqui trava
     printf("Thread retornando (Hidrogenio)\n");
   }
 
@@ -64,7 +64,7 @@ void* Hidrogenio(void* v) {
 
 int main() {
   srand(time(NULL));
-  int tamanho = MAX_THREADS-1;
+  int tamanho = MAX_THREADS - 1;
   pthread_t pthread[tamanho];
   sem_init(&sem_o, 1, 0);
   sem_init(&sem_h, 1, 0);
@@ -80,7 +80,7 @@ int main() {
 
     tamanho--;
   }
-  usleep(0.2*second);
+  usleep(0.2 * second);
   while (numero_o > 0) {
     sem_post(&sem_o);
     numero_o--;
@@ -89,15 +89,15 @@ int main() {
     sem_post(&sem_h);
     numero_h--;
   }
-  for (tamanho = MAX_THREADS-1; tamanho >= 0; tamanho--) {
-    pthread_join(pthread[tamanho], NULL);  
+  for (tamanho = MAX_THREADS - 1; tamanho >= 0; tamanho--) {
+    pthread_join(pthread[tamanho], NULL);
     printf("Join na thread %d\n", tamanho);
   }
 
   printf("Oxigenio produzido: %d\t Hidrogenio produzido: %d\t\n", o_gerado,
          h_gerado);
   int formado_esperado = h_gerado / 2;
-  if (o_gerado <= h_gerado / 2) {  // menos O do que H2
+  if (o_gerado <= h_gerado / 2) { // menos O do que H2
     formado_esperado = o_gerado;
   }
 
